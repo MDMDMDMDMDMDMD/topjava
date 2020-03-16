70696192918fd5175483b7c398220f9e156987db
MDMDMDMDMDMDMD <moskalevd@list.ru>
2020-03-16 11:47:54 +0300

    Add Servlet and jsp/html

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.topjava.web;
+
+import java.io.IOException;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+
+        request.getRequestDispatcher("/userList.jsp").forward(request,response);
+    }
+}
