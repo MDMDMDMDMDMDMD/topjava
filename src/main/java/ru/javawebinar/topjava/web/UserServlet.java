5d346a24d7fbcf5b1bda7229909f335ce133c44c
MDMDMDMDMDMDMD <moskalevd@list.ru>
2020-03-16 16:12:06 +0300

    1 5 add servlet api

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserServlet extends javax.servlet.http.HttpServlet {
+public class UserServlet extends HttpServlet {
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
-
-        request.getRequestDispatcher("/users.jsp").forward(request,response);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
